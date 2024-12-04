e23d09139605f4b76fa3d515f6a03290f4c3d3a7
venu-prasath <venuprasth133@gmail.com>
2024-12-03 22:49:13 -0500

    maps and sets explored

--- test/test.cpp
+++ test/test.cpp
@@ -48,10 +48,10 @@ main() {
 
     //sorting
 
-    vector<int> v = {4,2,5,3,5,8,3};
-    sort(v.begin(), v.end());
+    vector<int> vec = {4,2,5,3,5,8,3};
+    sort(vec.begin(), vec.end());
 
-    sort(v.rbegin(), v.rend());
+    sort(vec.rbegin(), vec.rend());
 
     int n = 5;
     int arr[] = {5,4,3,2,1};
@@ -65,7 +65,45 @@ main() {
   vv.push_back({1,5});
   vv.push_back({2,3});
   vv.push_back({1,2});
-  std::sort(vv.begin(), v.end())
-    
+  std::sort(vv.begin(), vv.end());
+
+  // sets
+  set<int> ss;
+  ss.insert(3);
+  ss.insert(2);
+  ss.insert(5);
+  std::cout << ss.count(3) << "\n";
+  std::cout << ss.count(4) << "\n";
+  ss.erase(3);
+  ss.insert(4);
+
+  std::cout << ss.count(3) << "\n";
+  std::cout << ss.count(4) << "\n";
+
+  set<int> s2 = {2, 5, 6, 8 };
+  std::cout << s2.size() << "\n";
+  for(auto x: s2) {
+    std::cout << x << "\n";
+  }
+
+  // unordered sets
+  unordered_set<int> s3;
+  s3.insert(3);
+  s3.insert(2);
+  s3.insert(5);
+  std::cout << s3.count(3) << "\n";
+  std::cout << s3.count(4) << "\n";
+  s3.erase(3);
+  s3.insert(4);
+
+  std::cout << s3.count(3) << "\n";
+  std::cout << s3.count(4) << "\n";
+
+  set<int> s4 = {2, 5, 6, 8 };
+  std::cout << s4.size() << "\n";
+  for(auto x: s4) {
+    std::cout << x << "\n";
+  }
+
 
 }
